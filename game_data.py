90b9373b16efa3042dc88830e4c17c961515ebcb
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-01-18 16:02:17 +0000

    fixes: change wording in question

--- game_data.py
+++ game_data.py
@@ -8,7 +8,7 @@ list_of_positions = {
     'G': {'positions': ('F', 'J'), 'directions': {'N': None, 'W': None, 'E': 'J', 'S': 'F'}, 'question': 'You have found the cat so you can move on. Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
     'H': {'positions': ('F'), 'directions': {'N': None, 'E': 'F', 'W': None, 'S': None}, 'question': 'You have found the cat! Great job! Choose a direction to escape: N, E, S or W ', 'isThereCat': True},
     'I': {'positions': ('F'), 'directions': {'N': 'F', 'W': None, 'E': None, 'S': None}, 'question': 'The cat is not in here. Please go back! Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
-    'J': {'positions': ('G', 'K', 'L', 'N'), 'directions': {'N': 'K', 'W': 'G', 'E': 'N', 'S': 'L'}, 'question': 'You are now on floor G. Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
+    'J': {'positions': ('G', 'K', 'L', 'N'), 'directions': {'N': 'K', 'W': 'G', 'E': 'N', 'S': 'L'}, 'question': 'You are now on ground floor. Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
     'K': {'positions': ('J', 'M'), 'directions': {'N': None, 'W': 'M', 'E': None, 'S': 'J'}, 'question': 'Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
     'L': {'positions': ('J', 'M'), 'directions': {'E': 'M', 'W': None, 'N': 'J', 'S': None}, 'question': 'There is no way out from here. Please go back! Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
     'M': {'positions': ('K', 'L'), 'directions': {'N': None, 'S': None, 'E': 'K', 'W': 'L'}, 'question': 'This is an empty room. Choose a direction to escape: N, E, S or W ', 'isThereCat': False},
